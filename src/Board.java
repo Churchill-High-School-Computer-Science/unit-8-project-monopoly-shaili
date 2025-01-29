67536dc4cabf2cecd3494e96a85bd9b8c05bce10
Shaili Desai <sdesai3287@stu.neisd.net>
2025-01-29 12:36:27 -0600

    drafttt todayyy

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,11 @@
-
+import java.awt.Color;
 class Board
 {
    Property[][] properties= new Property[10][10];
     public Board(){
         Display.properties= properties;
-        properties[0][0]= new Property(new Color(0,255,0), n:"Go" , chashing:0);
-        properties[0][1]= new Property(new Color(125,51,0), n:"Mediterranian" , chashing:60);
+        properties[0][0]= new Property(new Color(0,255,0), "Go" , 0);
+        properties[0][1]= new Property(new Color(125,51,0), "Mediterranian" , 60);
         properties[0][2]= new Property(new Color(0,255,0), n:"Community Chest" , chashing:0);
         properties[0][3]= new Property(new Color(102,51,0), n:"Baltic Avenue" , chashing:50);
         properties[0][1]= new Property(new Color(0,255,0), n:"Mediterranian" , chashing:60);
