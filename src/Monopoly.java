f2d96746e23c17c465c7ffddaf00a3f1cdf26652
Shaili Desai <sdesai3287@stu.neisd.net>
2025-01-31 12:39:47 -0600

    players add

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -2,8 +2,16 @@ import javax.swing.SwingUtilities;
 
 public class Monopoly {
     public static void main(String[] args) {
-        System.out.print("khfhbdjnf");
+        
         Board b= new Board();
+        Player p= new Player("silly", 1000);
+        Player o= new Player("Bob", 1000);
+        Player r= new Player("Sky", 1000);
+        Player s= new Player("Sam", 1000);
+        Display.players.add(p);
+        Display.players.add(o);
+        Display.players.add(r);
+        Display.players.add(s);
         //Graphics!
         SwingUtilities.invokeLater(() -> Display.setupFrame());
     }
--- src/Player.java
+++ src/Player.java
@@ -2,25 +2,35 @@
 import java.util.ArrayList;
 
 public class Player {
+    String name;
+    int money, locate;
+    ArrayList<Property> pp= new ArrayList<Property>();
+
+    public Player(String n, int mon)
+    {
+        name=n;
+        money=mon;
+        locate=0;
+    }
 
     //TODO FIX
     public String getName(){
-        return "no one";
+        return name;
     }
 
     ///TODO FIX
     public int getMoney(){
-        return 1000;
+        return money;
     }
 
     //TODO FIX
     public ArrayList<Property> getProperties(){
-        return null;
+        return pp;
     }
 
     //TODO FIX
     //Go is the top left corner, location 0. Locations increase by 1 for each property that the player passes clockwise.
     public int getLocation(){
-        return 0;
+        return locate;
     }
 }
